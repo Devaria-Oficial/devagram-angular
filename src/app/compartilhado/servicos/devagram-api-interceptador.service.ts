5066e2493fec786140313ed66283f3564ed43feb
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-06-29 20:05:32 -0300

    implementar loading e scroll nos comentarios

--- src/app/compartilhado/servicos/devagram-api-interceptador.service.ts
+++ src/app/compartilhado/servicos/devagram-api-interceptador.service.ts
@@ -1,12 +1,23 @@
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { delay, finalize, Observable } from "rxjs";
+import { LoadingService } from "../componentes/loading/loading.service";
 
 @Injectable({
     providedIn: 'root'
 })
 export class DevagramApiInterceptador implements HttpInterceptor {
+    private requisicoesEmAndamento: number = 0;
+    constructor(
+        private loadingService: LoadingService
+    ) {}
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        this.requisicoesEmAndamento++;
+        if (this.requisicoesEmAndamento === 1) {
+            this.loadingService.exibir();
+        }
+
         const token = localStorage.getItem('token');
         let novaReq = req;
         if (token) {
@@ -15,6 +26,14 @@ export class DevagramApiInterceptador implements HttpInterceptor {
             })
         }
 
-        return next.handle(novaReq);
+        return next.handle(novaReq).pipe(
+            delay(2000), // 2 segundos
+            finalize(() => {
+                this.requisicoesEmAndamento--;
+                if (this.requisicoesEmAndamento === 0) {
+                    this.loadingService.ocultar();
+                }
+            })
+        );
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  devagramUrlApi: 'https://devagram-nodejs-next.vercel.app/api'
+  devagramUrlApi: 'https://devagram-node-nextjs-kohl.vercel.app/api'
 };
